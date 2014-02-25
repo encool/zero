81d4dda482be1f9a264384baa88e99df055ea5cc
liqiaoone <liqiao@gmail.com>
2014-02-25 18:16:48 +0800

    .2

--- src/com/example/zero1/MainActivity.java
+++ src/com/example/zero1/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final ActionBar actionBar = getActionBar();
+//		final ActionBar actionBar = getActionBar();
 //		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 	}
 
