0b8412f00df6a5857547cb082658424608dc5d58
liqiaoone <liqiao@gmail.com>
2014-02-27 17:00:53 +0800

    0.7

---
+++ src/com/example/zero1/serchFragment.java
@@ -0,0 +1,11 @@
+package com.example.zero1;
+
+import android.support.v4.app.Fragment;
+
+public class serchFragment extends Fragment {
+
+	public serchFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
