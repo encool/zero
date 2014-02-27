a4cfde878eaca3ad0551aeaf2dae7e7bafbdb85c
liqiaoone <liqiao@gmail.com>
2014-02-27 18:16:32 +0800

    button_on_xml

---
+++ src/com/example/zero1/SerchFragment.java
@@ -0,0 +1,23 @@
+package com.example.zero1;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SerchFragment extends Fragment {
+
+	public SerchFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+//	@Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//			Bundle savedInstanceState) {
+//		// TODO Auto-generated method stub
+//		View rootview=new 
+//		return super.onCreateView(inflater, container, savedInstanceState);
+//	}
+
+}
