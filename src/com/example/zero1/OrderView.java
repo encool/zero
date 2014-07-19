88853d97cbdde254c12d3a59d025ca86e3f4a375
liqiaoone <liqiao100@gmail.com>
2014-07-19 23:06:10 +0800

    接下来需要处理  从网络上get order 数据

---
+++ src/com/example/zero1/OrderView.java
@@ -0,0 +1,48 @@
+package com.example.zero1;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class OrderView extends LinearLayout {
+	Order order;
+	
+	TextView startstation;
+	TextView arrivestaion;
+	TextView ordernum;
+	TextView passangername;
+	TextView date;
+	TextView time;
+	TextView seattype;
+	TextView seatnum;
+	
+	public OrderView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		LayoutInflater.from(context).inflate(R.layout.orderview_layout, this);
+	}
+
+	public OrderView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+	void initView(){
+		startstation=(TextView) this.findViewById(R.id.startstation);
+		arrivestaion=(TextView) this.findViewById(R.id.arrivestaion);
+		ordernum=(TextView) this.findViewById(R.id.ordernum);
+		passangername=(TextView) this.findViewById(R.id.passangername);
+		startstation=(TextView) this.findViewById(R.id.startstation);
+		date=(TextView) this.findViewById(R.id.date);
+		time=(TextView) this.findViewById(R.id.time);
+		seattype=(TextView) this.findViewById(R.id.seattype);
+		seatnum=(TextView) this.findViewById(R.id.seatnum);
+		
+	}
+
+	public void setOrderinfo(Order order) {
+		this.order = order;
+	}
+
+}
