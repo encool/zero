02a5bda722ffaad4b1818f1db0e776e125368daa
liqiao <liqiao>
2014-10-08 09:51:22 +0800

    sync

--- src/com/example/zero1/TrainsInfoActivity.java
+++ src/com/example/zero1/TrainsInfoActivity.java
@@ -7,6 +7,7 @@ import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
@@ -33,11 +34,20 @@ public class TrainsInfoActivity extends Activity {
 		//设置adapter
 		ListView listview=(ListView) findViewById(R.id.trainlist);
 		listview.setOnTouchListener(new OnTouchListener(){
-
+			
+			
 			@Override
 			public boolean onTouch(View arg0, MotionEvent arg1) {
 				// TODO Auto-generated method stub
 				// TODO Auto-generated method stub
+				Handler mHideHandler = new Handler();
+				Runnable mHideRunnable = new Runnable() {
+					@Override
+					public void run() {
+						mPopupWindow.dismiss();
+						hasdelaytask=false;
+					}
+				};
 				switch (arg1.getAction()) {
 					case MotionEvent.ACTION_MOVE:	//按下监听
 						View view = getLayoutInflater().inflate(R.layout.activity_trains_info_control, null);
@@ -51,26 +61,24 @@ public class TrainsInfoActivity extends Activity {
 							button.setOnClickListener(new OnClickListener() {
 								@Override
 								public void onClick(View arg0) {
-									// TODO Auto-generated method stub
-									
+									// TODO Auto-generated method stub									
 								}});
 						}
-
-//							//mPopupWindow显示时设为消失
-//							if (mPopupWindow.isShowing()) {
-//								mPopupWindow.dismiss();
-//							}
-
-							//设置mPopupWindow为显示，并设置位置
+						//设置mPopupWindow为显示，并设置位置
 						mPopupWindow.showAtLocation(findViewById(R.id.trainlist),
 									Gravity.RIGHT | Gravity.BOTTOM, 0, 0);		
 						break;
 					case MotionEvent.ACTION_UP: //抬起监听
+						if(!hasdelaytask){
+							mHideHandler.postDelayed(mHideRunnable, 3000);
+							hasdelaytask=true;
+						}
+							
 						break;
-
 					default:
 						break;
 				}
+
 				//注意：这里要返回true
 				return true;
 			}});
@@ -112,6 +120,15 @@ public class TrainsInfoActivity extends Activity {
 	 
 	}
 	private PopupWindow mPopupWindow;
+	boolean hasdelaytask=false;
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		mPopupWindow.dismiss();
+		hasdelaytask=false;
+		super.onPause();
+	}
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		// TODO Auto-generated method stub
